f0be92831bc9baf983934de5c9ead59818b3d523
Ray Jenkins <reofax@gmail.com>
2016-11-20 18:49:38 -0700

    Initial pass, all tests are green

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,30 +31,60 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('all RSS Feeds have a non empty defined url', function () {
+            for(i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].url).toBeDefined;
+                expect(allFeeds[i].url).not.toBe('');
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('all RSS Feeds have a non empty defined name', function () {
+            for(i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].name).toBeDefined;
+                expect(allFeeds[i].name).not.toBe('');
+            }
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('The menu is hidden by default', function() {
+            expect($("body").hasClass('menu-hidden')).toBe(true);
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('The menu displays when it is clicked, and hide when clicked again', function() {
+            expect($("body").hasClass('menu-hidden')).toBe(true);
+            $('.menu-icon-link').trigger( "click" );
+            expect($("body").hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').trigger( "click" );
+            expect($("body").hasClass('menu-hidden')).toBe(true);
+          });
+      });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -62,11 +92,37 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('After loadFeed there is at least a single .entry in the .feed container', function() {
+            expect($('.feed').length).toBeGreaterThan(0);
+            console.log('before');
+            console.log($('.entry-link').first().text());
+        });
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* TODO: Write a new test suite named "New Feed Selection"*/
+    describe('New Feed Selection', function() {
+        //Start the  index at -1 so that the firt spec loads index 0, and the second spec loads index 1
+        var index = -1;
+        var firstEntryText;
+        beforeEach(function(done) {
+            index = index + 1;
+            loadFeed(index, done);
+        });
+
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('loads text from the entry feed for index 0', function() {
+            firstEntryText = $('.entry-link').first().text();
+            expect(firstEntryText.length).toBeGreaterThan(0);
+            console.log(firstEntryText);
+        });
+
+        it('loads different text for the next index', function () {
+            expect($('.entry-link').first().text()).not.toBe(firstEntryText);
+        })
+    });
+
 }());
